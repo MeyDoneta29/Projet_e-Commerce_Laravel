859e62e6d32bb539cb3c58a368ce981cb1ee7081
Lulu <bouange45@gmail.com>
2026-01-14 16:47:09 +0000

    Mise en place du schema de la base de donnee et les models

---
+++ app/Models/order_item.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\order;
+use App\Models\product;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class order_item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'unit_price'];
+
+    public function order(){
+        return $this->belongsTo(order::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(product::class);
+    }
+
+}
